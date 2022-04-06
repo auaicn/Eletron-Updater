b133a7b3f6bdeb931e0abd428a481b3669f8a9c8
KyungHo Park <crorowslowpp@gmail.com>
2022-04-06 15:43:37 +0900

    Feat: tried using multiple window
    - it seems, 1 process is allocated by 1 window

--- desktop-app/main.js
+++ desktop-app/main.js
@@ -14,3 +14,27 @@ const createWindow = () => {
   }, 5000);
 };
 
+const win2 = new BrowserWindow({
+  width: 800,
+  height: 600,
+  webPreferences: {
+    preload: path.join(__dirname, "preload.js"),
+  },
+});
+console.log("main process is running");
+console.log(__dirname);
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // TODO - anyway, how to activate ?
+  // 설치파일이 있으면, 가능한것으로 보인다.
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  // macOS platform 을 darwin 으로 부르는 듯 하다.
+  if (process.platform !== "darwin") app.quit();
+});
